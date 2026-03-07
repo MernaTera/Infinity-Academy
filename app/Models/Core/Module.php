db1b21477435c391ffd4ead93b7bf085b1bcd4b9
merna Tera <mernatera@live.com>
2026-03-07 22:08:00 +0200

    Models: Added Core models

---
+++ app/Models/Core/Module.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Models\Core;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Module
+ * 
+ * @property int $module_id
+ * @property string|null $module_name
+ * @property Carbon|null $created_at
+ * 
+ * @property Collection|Permission[] $permissions
+ *
+ * @package App\Models
+ */
+class Module extends Model
+{
+	protected $table = 'module';
+	protected $primaryKey = 'module_id';
+	public $timestamps = false;
+
+	protected $fillable = [
+		'module_name'
+	];
+
+	public function permissions()
+	{
+		return $this->hasMany(Permission::class, 'module_id');
+	}
+	
+	public function scopeWithPermissions($query)
+	{
+    	return $query->with('permissions');
+	}
+}
